2ef16f99f08452eccfaa62359bc499e638366da0
mpioski <matheus.pioski@gmail.com>
2018-07-31 21:25:57 -0300

    Convertendo string para int.

---
+++ Aulas/Aula3107_Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Aula3107_Console{
+
+    class Program {
+        public static int id { get; set; }
+        static void Main(string[] args) {
+            Console.WriteLine("Digite um número: ");
+            id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Você digitou: " + id);
+            Console.ReadKey();
+        }
+    }
+}
