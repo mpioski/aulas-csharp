cb71b4583e6011598902736ea29a455bfcf3542d
mpioski <matheus.pioski@gmail.com>
2018-07-31 21:58:32 -0300

    While e if

--- Aulas/Aula3107_Console/Program.cs
+++ Aulas/Aula3107_Console/Program.cs
@@ -3,13 +3,36 @@
 namespace Aula3107_Console{
 
     class Program {
-        public static int id { get; set; }
+        public static int num1 { get; set; }
+        public static int num2 { get; set; }
         static void Main(string[] args) {
             Console.WriteLine("Digite um número: ");
-            id = int.Parse(Console.ReadLine());
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite outro número: ");
+            num2 = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Você digitou: " + id);
-            Console.ReadKey();
+            int opcao = 0;
+            while (opcao != 5) {
+
+                Console.WriteLine("Digite 1 para soma.\nDigite 2 para subtração.\nDigite 3 para multiplicação.\n" +
+                "Digite 4 para divisão.\nDigite 5 para sair.");
+
+                opcao = int.Parse(Console.ReadLine());
+                if (opcao == 1) {
+                    Console.WriteLine("A soma é: " + (num1 + num2));
+                } else if (opcao == 2) {
+                    Console.WriteLine("A subtração é: " + (num1 - num2));
+                } else if (opcao == 3) {
+                    Console.WriteLine("A multiplicação é: " + (num1 * num2));
+                } else if (opcao == 4) {
+                    Console.WriteLine("A divisão é: " + (num1 / num2));
+                } else if (opcao == 5) {
+                    break;
+                } else {
+                    Console.WriteLine("Opção inválida");
+                }
+            }
         }
+
     }
 }
